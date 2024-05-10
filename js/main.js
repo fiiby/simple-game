f40be77bd48e68b1dc92beb44241ef8834b31a3f
unknown <toohanour@gmail.com>
2024-05-10 22:07:10 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function character(name,power, life) {
+    this.name = name;
+    this.power = power;
+    this.life = life;
+}
+
+let naruto =new character('naruto',10,100);
+let saski =  new character('sasaki', 10 ,100);
+
+console.log(saski);
