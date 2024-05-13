79bf68c7832407247e4b75657f4b1ee70de7280b
unknown <toohanour@gmail.com>
2024-05-13 15:56:48 +0300

    more functionality

--- js/main.js
+++ js/main.js
@@ -1,49 +1,132 @@
-function character(name,power, life) {
-    this.name = name;
-    this.power = power;
-    this.life = life;
-}
-character.prototype.attack = function(opponent) {
-    //console.log(this);
-    //console.log(opponent);
+//this code not working: 
+//document.addEventListener('DOMContentLoaded', function() {
+// function character(name,strength, health) {
+//     this.name = name;
+//     this.strength= strength;
+//     this.health = health;
 
-    opponent.life -= this.power;
-    console.log(opponent.life);
+//     this.attackBtn = document.querySelector(`#${this.name}-attack `);
+//     this.healthBtn = document.querySelector(`#${this.name}-make-health`);
+//     this.progress = document.querySelector(`.${this.name}-health span`);
 
-}
-character.prototype.status = function() {
-    console.log(`name: ${this.name}`);
-    console.log(`power: ${this.power}`);
-    console.log(`life: ${this.life}` );
 
-}
+// }
 
-character.prototype.addLife = function(){
-    if(this.life < 100) {
-        this.life +=10;
+// character.prototype.attack = function(opponent) {
+ 
+//     opponent.health -= this.strength;
+    
+
+// }
+
+
+// character.prototype.status = function() {
+//     console.log(`Name: ${this.name}`);
+//     console.log(`Strength: ${this.strength}`);
+//     console.log(`Health: ${this.health}` );
+
+// }
+
+// character.prototype.makeHealth = function(){
+//     if(this.health < 100) {
+//         this.health +=10;
+//         this.progress.style.width = `${opponent.health}%`;
+//     }
+
+//         // in case life added become more than 100 condition:
+//       if(this.health > 100) {
+//         this.health = 100;
+//       }
+
+//     // Update the life bar visually
+//     // this.lifeBar.style.width = `${this.life}%`;
+   
+// }
+
+// let naruto =new character('naruto',10,100);
+// let sasaki =  new character('sasaki', 10 ,100);
+
+// console.log(sasaki);
+
+// naruto.attackBtn.addEventListener('click', function() {
+//     naruto.attack(sasaki);
+//     sasaki.status();
+//     console.log("error");
+// })
+
+// sasaki.attackBtn.addEventListener('click', function() {
+//     sasaki.attack(naruto);
+//     naruto.status();
+//     // console.log(`naruto life remain is ${naruto.status()}`);
+// })
+
+// naruto.healthBtn.addEventListener('click', function() {
+//     naruto.makeHealth();
+// })
+
+// sasaki.healthBtn.addEventListener('click', function() {
+//     sasaki.makeHealth();
+// })
+
+// });
+
+///////////////////////////////////////////////////////
+
+document.addEventListener('DOMContentLoaded', function() {
+    function Character(name, strength, health) {
+        this.name = name;
+        this.strength= strength;
+        this.health = health;
+
+        this.attackBtn = document.querySelector(`#${this.name}-attack`);
+        this.healthBtn = document.querySelector(`#${this.name}-make-health`);
+        this.progress = document.querySelector(`.${this.name}-health span`);
     }
 
-        // in case life added become more than 100 condition:
-      if(this.life > 100) {
-        this.life = 100;
-      }
-    //     return(life);
-    // }
-}
+    Character.prototype.attack = function(opponent) {
+        opponent.health -= this.strength;
+    }
 
-let naruto =new character('naruto',10,100);
-let sasaki =  new character('sasaki', 10 ,100);
+    Character.prototype.status = function() {
+        console.log(`Name: ${this.name}`);
+        console.log(`Strength: ${this.strength}`);
+        console.log(`Health: ${this.health}` );
+    }
 
-sasaki.attack(naruto);
-sasaki.attack(naruto);
+    Character.prototype.makeHealth = function(){
+        if(this.health < 100) {
+            this.health +=10;
+            this.progress.style.width = `${this.health}%`;
+        }
 
-console.log(naruto);
+        // Ensure health doesn't exceed 100
+        if(this.health > 100) {
+            this.health = 100;
+        }
+    }
 
-naruto.addLife();
+    let naruto = new Character('naruto', 10, 100);
+    let sasaki = new Character('sasaki', 10, 100);
 
-naruto.status();
+    console.log(sasaki);
 
-// naruto.attack(saski);
-// naruto.attack(saski);
-// naruto.attack(saski);
-// naruto.attack(saski);
+    naruto.attackBtn.addEventListener('click', function() {
+        naruto.attack(sasaki);
+        sasaki.status();
+        console.log("error");
+    })
+
+    sasaki.attackBtn.addEventListener('click', function() {
+        sasaki.attack(naruto);
+        naruto.status();
+    })
+
+    naruto.healthBtn.addEventListener('click', function() {
+        naruto.makeHealth();
+    })
+
+    sasaki.healthBtn.addEventListener('click', function() {
+        sasaki.makeHealth();
+    })
+
+});
